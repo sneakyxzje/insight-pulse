da809cda398453b9efa4ec6c24b663d8818da0b9
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-15 01:36:07 +0700

    chore(config): update path aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,13 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import path from "node:path";
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      $lib: path.resolve(__dirname, "./src/lib"),
+    },
+  },
+});
