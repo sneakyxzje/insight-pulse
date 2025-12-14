c082caa4336069d7348edc4d77c9115f4d715012
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-14 17:08:54 +0700

    feat(security): add authentication manager bean

--- server/src/main/java/com/insight_pulse/tech/security/config/SecurityConfig.java
+++ server/src/main/java/com/insight_pulse/tech/security/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -66,4 +68,10 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 }
