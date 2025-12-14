a835d37f209cda55040779b4c2d32af310e1c6bd
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-15 01:23:02 +0700

    chore: move file to correct directory

--- server/src/main/java/com/insight_pulse/tech/auth/dtos/login/LoginRequest.java
+++
@@ -1,5 +0,0 @@
-package com.insight_pulse.tech.auth.dtos.login;
-
-public record LoginRequest(String email, String password) {
-    
-}
--- server/src/main/java/com/insight_pulse/tech/auth/dtos/register/RegisterResponse.java
+++
@@ -1,7 +0,0 @@
-package com.insight_pulse.tech.auth.dtos.register;
-
-public record RegisterResponse(
-    int id
-) {
-    
-}
