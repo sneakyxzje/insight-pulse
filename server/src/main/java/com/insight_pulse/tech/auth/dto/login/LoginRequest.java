b6f7bcb5eb7cb4053d9e1c467074de7f54f66b31
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-15 01:24:23 +0700

    chore: move to correct directory

---
+++ server/src/main/java/com/insight_pulse/tech/auth/dto/login/LoginRequest.java
@@ -0,0 +1,5 @@
+package com.insight_pulse.tech.auth.dto.login;
+
+public record LoginRequest(String email, String password) {
+    
+}
---
+++ server/src/main/java/com/insight_pulse/tech/auth/dto/login/LoginResponse.java
@@ -0,0 +1,5 @@
+package com.insight_pulse.tech.auth.dto.login;
+
+public record LoginResponse(String message) {
+    
+}
