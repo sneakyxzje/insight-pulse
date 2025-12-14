9eed67793b7ac72e1afd6a28f0626ef08a6bee8d
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-15 01:35:42 +0700

    chore(config): update path aliases

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -13,7 +13,9 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
     alias: {
-      "@/*": "./src/lib/*",
+      $lib: "./src/lib",
+      "@": "./src",
+      "@src": "./src",
     },
   },
 };
