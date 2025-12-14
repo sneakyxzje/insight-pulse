f5e71abf12dc00a3ba4f45d94e11fd2711250836
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-14 17:07:20 +0700

    feat(user): add method to check if email exists

--- server/src/main/java/com/insight_pulse/tech/user/domain/UserRepository.java
+++ server/src/main/java/com/insight_pulse/tech/user/domain/UserRepository.java
@@ -4,6 +4,6 @@ package com.insight_pulse.tech.user.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    
+    boolean existsByEmail(String email);    
     User findByEmail(String email);
 }
