a57bff5a1b43c5cf1a6f6dfa84de702b0baa087a
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-16 17:33:59 +0700

    chore: relocate file

---
+++ client/src/routes/(app)/campaigns/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Sidebar from "@src/lib/components/Sidebar.svelte";
+  import * as Sidebars from "$lib/components/ui/sidebar/index.js";
+  let { children } = $props();
+</script>
+
+<Sidebars.Provider>
+  <Sidebar />
+  <main class="container mx-auto py-8">
+    {@render children()}
+  </main>
+</Sidebars.Provider>
