6709b75200e53ed33835c3f1ca0d5010a5f574ad
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-17 17:56:50 +0700

    feat(submission): implement find all submission, find submission by campaign and user

--- server/src/main/java/com/insight_pulse/tech/submission/domain/SubmissionRepository.java
+++ server/src/main/java/com/insight_pulse/tech/submission/domain/SubmissionRepository.java
@@ -1,7 +1,25 @@
 package com.insight_pulse.tech.submission.domain;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SubmissionRepository extends JpaRepository<Submission, String> {
-    
+    List<Submission> findAllByCampaignId(String campaignId);
+
+    @Query("""
+        SELECT s
+        FROM Submission s
+        WHERE s.campaign.id = :campaignId
+        AND s.campaign.user.id = :userId
+    """)
+    List<Submission> findByCampaignAndUser(
+        @Param("campaignId") String campaignId,
+        @Param("userId") int userId
+    );
+
+    long countByCampaign_Id(String campaignId);
 }
