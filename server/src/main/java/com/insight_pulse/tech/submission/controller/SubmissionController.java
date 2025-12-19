d9fc30a2cd7467beb7c6bf3ae8ad1f290f21e61e
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-19 17:46:57 +0700

    feat(submission): expose public campaign schema endpoint

--- server/src/main/java/com/insight_pulse/tech/submission/controller/SubmissionController.java
+++ server/src/main/java/com/insight_pulse/tech/submission/controller/SubmissionController.java
@@ -1,12 +1,15 @@
 package com.insight_pulse.tech.submission.controller;
 
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.insight_pulse.tech.campaign.dto.PublicCampaignResponse;
 import com.insight_pulse.tech.submission.dto.SubmissionRequest;
 import com.insight_pulse.tech.submission.service.SubmissionService;
 
@@ -18,9 +21,15 @@ import lombok.RequiredArgsConstructor;
 public class SubmissionController {
  
     private final SubmissionService submissionService;
+
     @PostMapping("/{campaignId}")
     public ResponseEntity<String> submitForm(@PathVariable String campaignId, @RequestBody SubmissionRequest request) {
         submissionService.submitForm(campaignId, request);
         return ResponseEntity.ok("Submit thành công");
+    }
+
+    @GetMapping("/campaign-schema/{campaignId}")
+    public ResponseEntity<PublicCampaignResponse> getPublicSchema(@PathVariable String campaignId) {
+        return ResponseEntity.ok(submissionService.getPublicSchema(campaignId));
     }
 }
