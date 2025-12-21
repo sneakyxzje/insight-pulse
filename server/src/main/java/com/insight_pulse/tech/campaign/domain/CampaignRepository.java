f03a08d3061bf021921abfec0896b8d4da28bb85
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-21 17:34:26 +0700

    feat(campaign): add pageable support and incrementTotalSubmissions method in repository

--- server/src/main/java/com/insight_pulse/tech/campaign/domain/CampaignRepository.java
+++ server/src/main/java/com/insight_pulse/tech/campaign/domain/CampaignRepository.java
@@ -1,12 +1,20 @@
 package com.insight_pulse.tech.campaign.domain;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 public interface CampaignRepository extends JpaRepository<Campaign, String> {
     
-    List<Campaign> findAllByUserId(int userId);
+    Page<Campaign> findAllByUserId(int userId, Pageable pageable);
 
     Optional<Campaign> findByIdAndUserId(String id, int userId);
+
+    @Modifying 
+    @Query("UPDATE Campaign c SET c.totalSubmissions = c.totalSubmissions + 1 WHERE c.id = :id")
+    void incrementTotalSubmissions(@Param("id") String id);
 }
