051468fc100d2ba90e3301700ea872d607a1bea5
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-23 23:30:08 +0700

    feat(auth): implement access/refresh token flow with cookie-based login, refresh, and logout

--- server/src/main/java/com/insight_pulse/tech/auth/service/AuthService.java
+++ server/src/main/java/com/insight_pulse/tech/auth/service/AuthService.java
@@ -1,4 +1,7 @@
 package com.insight_pulse.tech.auth.service;
+import java.util.List;
+
+
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -28,7 +31,7 @@ public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
 
     private final AuthenticationManager authenticationManager;
-    
+
     public RegisterResponse register(RegisterRequest request) {
 
         if(userRepository.existsByEmail(request.email())) {
@@ -43,29 +46,77 @@ public class AuthService {
         return new RegisterResponse(saved.getId());
     }   
 
-    public ResponseCookie login(LoginRequest request) {
+    public List<ResponseCookie> login(LoginRequest request) {
         Authentication authentication =  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         int userId = userDetails.getId();
         String role = authentication.getAuthorities().iterator().next().getAuthority();
         String token = jwtTokenProvider.generateToken(userId, role);
-        return ResponseCookie.from("jwt", token)
-        .httpOnly(true)
-        .secure(false) 
-        .path("/")
-        .maxAge(24 * 60 * 60)
-        .sameSite("Strict")
-        .build();
+        String refreshToken = jwtTokenProvider.generateRefreshToken();
+        jwtTokenProvider.storeToken(String.valueOf(userId), refreshToken);
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refresh_token", refreshToken)
+                .httpOnly(true)
+                .secure(false) 
+                .path("/")
+                .maxAge(7 * 24 * 60 * 60) 
+                .sameSite("Strict") 
+                .build();
+
+            ResponseCookie accessTokenCookie = ResponseCookie.from("jwt", token)
+                .httpOnly(true)
+                .secure(false) 
+                .path("/")
+                .maxAge(jwtTokenProvider.getExpiryInSeconds())
+                .sameSite("Strict")
+                .build();
+            return List.of(refreshTokenCookie, accessTokenCookie);
     }
 
-    public ResponseCookie logout() {
-        return ResponseCookie.from("jwt")
-        .httpOnly(true)
-        .secure(false) 
-        .path("/")
-        .maxAge(0)
-        .sameSite("Strict")
-        .build();
+    /* 
+        Return về 1 list, chứa 2 cái AT và RT cho client
+    */
+    public List<ResponseCookie> refreshToken(String refreshToken) {
+        String userIdStr = jwtTokenProvider.getUserIdFromRefreshToken(refreshToken);
+        if (userIdStr == null) {
+            throw new RuntimeException("userId not found");
+        }
+
+        Integer userId = Integer.parseInt(userIdStr);
+        User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new RuntimeException("User không tồn tại"));
+        String role = user.getRole().name();
+
+        jwtTokenProvider.deleteRefreshToken(refreshToken);
+
+        String newAccessToken = jwtTokenProvider.generateToken(userId, role);
+        String newRefreshToken = jwtTokenProvider.generateRefreshToken();
+
+        jwtTokenProvider.storeToken(userIdStr, newRefreshToken);
+
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refresh_token", newRefreshToken)
+                .httpOnly(true)
+                .secure(false) 
+                .path("/")
+                .maxAge(7 * 24 * 60 * 60) 
+                .sameSite("Strict") 
+                .build();
+
+        ResponseCookie accessTokenCookie = ResponseCookie.from("jwt", newAccessToken)
+                .httpOnly(true)
+                .secure(false) 
+                .path("/")
+                .maxAge(24 * 60 * 60)
+                .sameSite("Strict")
+                .build();
+        return List.of(refreshTokenCookie, accessTokenCookie);
+    }
+
+    public List<ResponseCookie> logout(String refreshToken) {
+        jwtTokenProvider.deleteRefreshToken(refreshToken);
+        ResponseCookie jwtCookie = ResponseCookie.from("jwt", "").path("/").maxAge(0).build();
+        ResponseCookie rtCookie = ResponseCookie.from("refresh_token", "").path("/").maxAge(0).build();
+    
+    return List.of(jwtCookie, rtCookie);
     }
 }
