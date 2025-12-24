ffb5afc65bacc906bbe755c31a4f11f8601f6759
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-24 15:58:08 +0700

    feat(auth): auto refresh access token in handle hook using refresh token

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -1,16 +1,21 @@
 import { getMe } from "@src/lib/api/user";
+import { syncAuthCookie } from "@src/lib/utils/auth";
 import { redirect } from "@sveltejs/kit";
 
 export const handle = async ({ event, resolve }) => {
-  const session = event.cookies.get("jwt");
-
-  if (session) {
+  const jwt = event.cookies.get("jwt");
+  const refreshToken = event.cookies.get("refresh_token");
+  if (jwt) {
     try {
       const user = await getMe(event.fetch);
       event.locals.user = user;
-    } catch (error) {
-      event.locals.user = null;
+    } catch (error: any) {
+      if ((error.status === 401 || error.status === 403) && refreshToken) {
+        await tryToRefresh(event);
+      }
     }
+  } else if (refreshToken) {
+    await tryToRefresh(event);
   }
   const isAppRoute = event.route.id?.includes("(app)");
 
@@ -28,3 +33,22 @@ export const handle = async ({ event, resolve }) => {
   }
   return resolve(event);
 };
+
+async function tryToRefresh(event: any) {
+  try {
+    const refreshRes = await event.fetch(
+      "http://localhost:8080/api/auth/refresh",
+      { method: "POST" }
+    );
+
+    if (refreshRes.ok) {
+      syncAuthCookie(refreshRes, event);
+      const user = await getMe(event.fetch);
+      event.locals.user = user;
+    } else {
+      event.locals.user = null;
+    }
+  } catch (err) {
+    event.locals.user = null;
+  }
+}
