165d24a7bf5bff945094cc05c20c5cbde9916607
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-24 15:59:34 +0700

    feat(campaign): add campaign layout

---
+++ client/src/routes/(app)/campaigns/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let { children } = $props();
+</script>
+
+<main
+  class="mr-3 my-3 h-[calc(100svh-1.5rem)] rounded-lg border border-sidebar-border bg-white px-8 py-8 overflow-hidden shadow-sm overflow-auto"
+>
+  {@render children()}
+</main>
