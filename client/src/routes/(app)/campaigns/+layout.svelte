705582af6eb6bfdd358d7ab2875e628d8a7bd1e2
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-26 16:04:46 +0700

    feat(campaign): add breadcrumb header and update main layout styles

--- client/src/routes/(app)/campaigns/+layout.svelte
+++ client/src/routes/(app)/campaigns/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+  import BreadcrumbComponent from "@src/lib/components/BreadcrumbComponent.svelte";
+
   let { children } = $props();
 </script>
 
 <main
-  class="mr-3 my-3 h-[calc(100svh-1.5rem)] rounded-lg border border-sidebar-border bg-white px-8 py-8 overflow-hidden shadow-sm overflow-auto"
+  class="mr-3 my-2 h-[calc(100svh-1rem)] rounded-lg border border-base-border-1 bg-base-2 overflow-hidden shadow-sm overflow-auto"
 >
-  {@render children()}
+  <div class="border-base-border-1 flex-none border-b">
+    <BreadcrumbComponent />
+  </div>
+  <div class="py-4 px-4 flex-1 overflow-y-auto">
+    {@render children()}
+  </div>
 </main>
