51a5ecec1068192852ab89d5f356e5dc4ed6389d
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-26 16:20:43 +0700

    feat(user): add find by google id method

--- server/src/main/java/com/insight_pulse/tech/user/domain/UserRepository.java
+++ server/src/main/java/com/insight_pulse/tech/user/domain/UserRepository.java
@@ -1,9 +1,12 @@
 package com.insight_pulse.tech.user.domain;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByEmail(String email);    
     User findByEmail(String email);
+    Optional<User> findByGoogleId(String googleId);
 }
